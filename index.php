eac944bbf43f592c789f7be490f3dc38155fb79d
Erty Seidel <ertysdl+github@gmail.com>
2013-09-05 18:30:52 -0600

    Starting to build structures
    Created some objects to keep track of followed links. My plan is to then
    traverse the graph to find the highest weighted set of three nodes.

--- index.php
+++ index.php
@@ -1,11 +1,52 @@
 <?php
-	$file = fopen('./apache.log', 'r');
+	class Connection{
+		public $count = 1;
+	}
+
+	class Node{
+		public $Connections = array();
+		function addConnection($pageName){
+			if(isset($this->Connections[$pageName])){
+				$this->Connections[$pageName]->count ++;
+			} else{
+				$this->Connections[$pageName] = new Connection();
+			}
+		}
+	}
+
+	class User{
+		public $location;
+		function __construct($location){
+			$this->location = &$location;
+		}
+	}
+
+	$file = fopen('./apache.log.test', 'r');
 	if(!$file) die('Could not find apache.log');
-	$info = array();
+
+	$users = array();
+	$nodes = array();
+
 	while(($line = fgets($file)) !== false){
+		$info = array();
 		preg_match('/^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) .*"GET (\/.*\/) HTTP/', $line, $info);
 		$ip = $info[1];
-		$page = $info[2];
-		echo($ip . ' visited '. $page . '<br />');
+		$pageName = $info[2];
+		if(isset($nodes[$pageName])){
+			$page = $nodes[$pageName];
+		} else{
+			$page = new Node();
+			$nodes[$pageName] = $page;
+		}
+		
+		if(isset($users[$ip])){
+			$users[$ip]->location->addConnection($pageName);
+			$users[$ip]->location = &$page;
+		} else{
+			$users[$ip] = new User($page);
+		}
 
 	}
+
+	//print_r($users);
+	print_r($nodes);
