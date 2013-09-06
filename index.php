b25044a6b1c768573a545170998487e3a718352f
Erty Seidel <ertysdl+github@gmail.com>
2013-09-06 12:33:38 -0600

    Fixed doing it wrong
    Realized I was computing the wrong thing - rewrote everything. This was
    actually a lot faster to solve now that I knew what I was supposed to be
    doing!

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 
-	class Node{
-		public $links = array();
-		function addConnection($pageName){
-			if(isset($this->links[$pageName])){
-				$this->links[$pageName] ++;
+	class User{
+		public $path = array(0=>"", 1=>"", 2=>"");
+		function addVisit($pageName, &$visits){
+			$this->path[0] = $this->path[1];
+			$this->path[1] = $this->path[2];
+			$this->path[2] = $pageName;
+			$pathString = '"' . $this->path[0] . '", "' . $this->path[1] . '", ' . $this->path[2] . '"';
+			if(isset($visits[$pathString])){
+				$visits[$pathString] ++;
 			} else{
-				$this->links[$pageName] = 1;
+				$visits[$pathString] = 1;
 			}
 		}
 	}
@@ -15,31 +19,29 @@
 	if(!$file) die('Could not find apache.log');
 
 	$users = array();
-	$nodes = array();
+	$visits = array();
 
 	while(($line = fgets($file)) !== false){
 		$info = array();
-		preg_match('/^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) .*"GET (\/.*\/) HTTP/', $line, $info);
+		preg_match('/^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) .*"GET (\/.*\/) H/', $line, $info);
 		$ip = $info[1];
 		$pageName = $info[2];
-
-		if(isset($nodes[$pageName])){
-			$page = $nodes[$pageName];
-		} else{
-			$page = new Node();
-			$nodes[$pageName] = $page;
-		}
 		
 		if(isset($users[$ip])){
-			$users[$ip]->addConnection($pageName);
-			$users[$ip] = $page;
+			$users[$ip]->addVisit($pageName, $visits);
 		} else{
-			$users[$ip] = $page;
-		}	
-
-		echo($ip . ", " . $pageName . "\r\n");
-
+			$users[$ip] = new User();
+			$users[$ip]->addVisit($pageName, $visits);
+		}
 	}
 
-	//print_r($users);
-	print_r($nodes);
+	$maxCount = 0;
+	$maxPath = "";
+	foreach($visits as $path => $visitCount){
+		if($visitCount > $maxCount){
+			$maxCount = $visitCount;
+			$maxPath = $path;
+		}
+	}
+
+	print("The path " . $maxPath . " was visited " . $maxCount . " times.");
